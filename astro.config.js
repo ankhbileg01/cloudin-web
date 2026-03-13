a78659c654cb1ee2a14702e0e927e59b7cd00402
ankhbileg01 <ankhbileg78@outook.com>
2026-03-13 14:26:11 +0800

    Initial commit

--- astro.config.js
+++ astro.config.js
@@ -6,8 +6,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ctrimm.github.io',
-  base: '/astro-genai-startup-theme',
+  site: 'https://ankbileg01.github.io',
+  base: '/cloudin-web/',
   integrations: [
     react(),
     tailwind({
